e87dd6406a49a5c31da87433fb8de6c89ec1ffc4
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-12 09:53:00 -0600

    Started Runner times program

---
+++ Runner.cpp
@@ -0,0 +1,17 @@
+//Program That recieves three different runner times then determines the winner
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+    string runner1, runner2, runner3;
+    float time1, time2, time3;
+
+    cout << "Name of Runner 1: ";
+    getline(cin,runner1);
+    cout << "Enter the runners time: ";
+    cin >> time1;
+}
+
