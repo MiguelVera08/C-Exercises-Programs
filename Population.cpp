0cd3f6f71e25affb4e0bb42298ecbc456a11f2e1
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-17 11:51:00 -0600

    Added private and public constructors

--- Population.cpp
+++ Population.cpp
@@ -6,4 +6,14 @@ using namespace std;
 
 class Population{
 
+    //Population and the annual number of births and deaths.
+    private:
+        long population;
+        double numBirths;
+        double numDeaths;
+
+    public:
+        //constructors
+        Pop();
+        Pop(long, double, double);
 };
