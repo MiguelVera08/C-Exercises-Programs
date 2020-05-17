136ce0c85d5130c08b910f5211792cbd4fc02be3
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-17 11:45:00 -0600

    Started Population class

---
+++ Population.cpp
@@ -0,0 +1,9 @@
+//Program using a class called Population to determine the birth and death rates of location the user inputs
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Population{
+
+};
