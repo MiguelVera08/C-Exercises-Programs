23b6520b815ab10d409b6d03d9c876cf7fb8433e
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-20 16:05:00 -0600

    Added Function prototypes

---
+++ Name.cpp
@@ -0,0 +1,12 @@
+//Program sorts an array of strings and then displays the list of names in order
+
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+//Function prototype
+void selectionSort(string [], int);
+void displayNames(const string [], int);
+int binarySearch(const string [], int, string);
