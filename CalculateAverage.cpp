8e73e02ae6ba693d9d6be6bac748f359a737397b
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-22 09:46:00 -0600

    Added function prototypes

---
+++ CalculateAverage.cpp
@@ -0,0 +1,11 @@
+//Program calculates average scores based on the inputs by user
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Function prototypes
+void selectionSort (double *, int);
+void showArray (double *, int);
+void scoreAverage (double, int);
+
