be5c6b08ca6f2651060b300614c38b837a5c69b1
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-24 10:52:00 -0600

    Added variables and arry that will hold quarterly sales

---
+++ DivisionSales.cpp
@@ -0,0 +1,11 @@
+
+#include "DivisionSales.h"
+#include <iostream>
+using namespace std;
+
+//Definition of the static member of DivSales class
+double DivisionSales::totalSales = 0.0;
+//Array will hold the division quarterly sales.
+void DivisionSales::setQuarters(double quarter1, double quarter2, double quarter3, double quarter4){
+
+}
