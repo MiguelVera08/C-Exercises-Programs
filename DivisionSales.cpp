73de7ec2e8add463c726b116f641657d1281a48d
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-24 10:54:00 -0600

    quick fix

--- DivisionSales.cpp
+++ DivisionSales.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-//Definition of the static member of DivSales class
+//Definition of the static member of DivisionSales class
 double DivisionSales::totalSales = 0.0;
 //Array will hold the division quarterly sales.
 void DivisionSales::setQuarters(double quarter1, double quarter2, double quarter3, double quarter4){
