3108b34e59aef949a4a5b4d32b70c7ef2c525e01
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-24 11:07:00 -0600

    Added for loop to add all quarter values to totalsales

--- DivisionSales.cpp
+++ DivisionSales.cpp
@@ -7,5 +7,16 @@ using namespace std;
 double DivisionSales::totalSales = 0.0;
 //Array will hold the division quarterly sales.
 void DivisionSales::setQuarters(double quarter1, double quarter2, double quarter3, double quarter4){
+    quarters[0] = quarter1;
+    quarters[1] = quarter2;
+    quarters[2] = quarter3;
+    quarters[3] = quarter4;
 
-}
+    //Add all the quarter values to the static TotalSales
+    for(int i =0; i < 4; i++)
+    {
+        totalSales += quarters[i];
+    }
+    //Add subtotal values for each division
+    total = quarter1 + quarter2 + quarter3 +quarter4;
+};
