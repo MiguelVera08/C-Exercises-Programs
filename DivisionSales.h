e4f7cf26499472218b8a873ef2260765d69a41b7
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-24 10:39:00 -0600

    Added public variables

--- DivisionSales.h
+++ DivisionSales.h
@@ -9,6 +9,12 @@ private:
     double quarters[4];         //Array to hold the quarter sales figures for the division
     static double totalSales;   //Variable holds total corporate sales for all divisions
 
-};
+public:
+    void setQuarters(double, double, double, double ); //Member function that stores quarter sales
+    double getQuarters(int);                           //Get the value of a element in the array
+    static double getTotalSales();                    //Get the total of the sales
+    double total;	                //Variable holds the subtotal of each division
 
+};
+#include "DivisionSales.cpp"
 #endif
