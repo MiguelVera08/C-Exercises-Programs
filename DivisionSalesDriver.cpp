d6a87c0e5ba5797f445f7001b550caca21534de0
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-25 10:22:00 -0600

    Started for loop to get sales values for each divisions quarters from user

--- DivisionSalesDriver.cpp
+++ DivisionSalesDriver.cpp
@@ -10,4 +10,13 @@ int main(){
 
     DivisionSales div[6];    //variables hold the six divisions and four quarters
     double qSale[4];
+
+    //Loops to get the sales values from user for each divisions quarters
+    for(int count =0; count < 6; count++){
+        cout << "Enter sales data for Division " << (count + 1) << "\t" << endl;
+        for(int i = 0; i < 4; i++){
+            cout << "\t" << "Quarter " << (i + 1) << ": ";
+            cin >> qSale[i];
+        }
+    }
 }
