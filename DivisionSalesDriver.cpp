33e6d33bb914ea59d281c3e4e4ffa087fe034e08
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-26 12:23:00 -0600

    Added variables for division sales of the six divisions and the quarters

---
+++ DivisionSalesDriver.cpp
@@ -0,0 +1,13 @@
+//Driver program for finding the quarterly sales of 6 divisions then displaying the total sales for each division
+
+#include <iostream>
+#include <iomanip>
+#include "DivisionSales.h"
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+    DivisionSales div[6];    //variables hold the six divisions and four quarters
+    double qSale[4];
+}
