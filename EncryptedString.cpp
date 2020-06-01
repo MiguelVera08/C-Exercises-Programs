063f030b685bda5f9dc8e2a029624fdd0b4d1241
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-01 10:36:00 -0600

    Created EncryptedString.cpp and EncryptedString Method

---
+++ EncryptedString.cpp
@@ -0,0 +1,10 @@
+#include "EncryptedString.h"
+#include <iostream>
+using namespace std;
+
+// Method set - encrypt the string and store in the object
+void EncryptedString::set(string aString){
+
+    myString = "";
+}
+
