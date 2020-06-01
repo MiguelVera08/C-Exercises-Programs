fa6346c2b1b5e76c00cec6ae88ee6d0e877a244f
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-01 10:14:00 -0600

    Created Encryptedstring header file

---
+++ EncryptedString.h
@@ -0,0 +1,13 @@
+#ifndef ENCRYPTEDSTRING_H
+#define ENCRYPTEDSTRING_H
+
+
+class EncryptedString{
+    public:
+    EncryptedString();
+    EncryptedString(string astring);
+
+};
+
+#include "EncryptedString.cpp"
+#endif
