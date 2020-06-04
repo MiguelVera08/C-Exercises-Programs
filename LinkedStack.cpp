fd312b3dc20122b6b54b2e98f077e86d56d5479c
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-04 13:16:00 -0600

    Created pointer to first node

--- LinkedStack.cpp
+++ LinkedStack.cpp
@@ -5,3 +5,20 @@ template<class ItemType>
 LinkedStack<ItemType>::LinkedStack() : topPtr(nullptr)
 {
 }  // end default constructor
+
+template<class ItemType>
+LinkedStack<ItemType>::LinkedStack(const LinkedStack<ItemType>& aStack){
+
+
+    // Point to nodes in original chain
+    Node<ItemType>* origChainPtr = aStack.topPtr;
+
+    if (origChainPtr == nullptr) {
+        topPtr = nullptr;  // Original stack is empty
+    }else{
+        // Copy first node
+        topPtr = new Node<ItemType>();
+        topPtr->setItem(origChainPtr->getItem());
+    }
+
+}
