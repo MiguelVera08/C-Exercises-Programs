b4a2fe014f86c31435ae42d4a7a44b8f23fa9018
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-09 09:21:00 -0600

    Adds new entry to top of stack

--- StackInterface.h
+++ StackInterface.h
@@ -8,4 +8,10 @@ public:
     /** Sees whether this stack is empty.
      @return True if the stack is empty, or false if not. */
     virtual bool isEmpty() const = 0;
+
+    /** Adds a new entry to the top of this stack.
+    @post If the operation was successful, newEntry is at the top of the stack.
+    @param newEntry The object to be added as a new entry.
+    @return True if the addition is successful or false if not. */
+    virtual bool push(const ItemType& newEntry) = 0;
 };
