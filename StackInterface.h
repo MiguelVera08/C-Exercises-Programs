cf6805c8294862ebcbfe124ecf2a281818f5d800
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-09 09:12:00 -0600

    Checks to see if stack is empty

--- StackInterface.h
+++ StackInterface.h
@@ -4,4 +4,8 @@
 template<class ItemType>
 class StackInterface{
 
+public:
+    /** Sees whether this stack is empty.
+     @return True if the stack is empty, or false if not. */
+    virtual bool isEmpty() const = 0;
 };
