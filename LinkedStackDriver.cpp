99edad5d521e8a90638bc73803bdd45a12392448
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-11 10:44:00 -0600

    Created linked stack driver

---
+++ LinkedStackDriver.cpp
@@ -0,0 +1,12 @@
+//Program will get operations and numbers form user and calculate outcomes using a linked stack
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <cctype>
+
+
+#include "LinkedStack.h"
+
+using namespace std;
