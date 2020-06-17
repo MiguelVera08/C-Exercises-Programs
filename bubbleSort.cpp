dfca8869a37fa4eae369870b968408e4a56cba3b
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-17 10:12:00 -0600

    added local variables and started while loop

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -7,3 +7,12 @@
 
 template<class ItemType>
 
+//Takes in an array as param, and the size of the array as second param
+void bubbleSort(ItemType theArray[], int n){
+
+    bool sorted = false; // False when swaps occur
+    int pass = 1;
+    while(!sorted && (pass < n)){
+
+    }
+}
