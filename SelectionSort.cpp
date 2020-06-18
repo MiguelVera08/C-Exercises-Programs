5564fde5115063f625c4c5d53575c6f7d85ed01f
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-18 10:14:00 -0600

    Startd selectionsort with explaination

---
+++ SelectionSort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+/** Finds the largest item in an array.
+ @pre  The size of the array is >= 1.
+ @post  The arguments are unchanged.
+ @param theArray  The given array.
+ @param size  The number of elements in theArray.
+ @return  The index of the largest entry in the array. */
+template<class ItemType>
+int findIndexOfLargest(const ItemType theArray[], int size);
+
+
+
+
