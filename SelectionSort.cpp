c3eca4ec5def2f0b4182fe2a0ace3cfac3b9431b
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-18 10:37:00 -0600

    For loop for sorting points

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -19,5 +19,19 @@ int findIndexOfLargest(const ItemType theArray[], int size);
 template<class ItemType>
 void selectionSort(ItemType theArray[], int n){
 
-}
+    // last = index of the last item in the subarray of items yet
+    //        to be sorted;
+    // largest = index of the largest item found
+    for (int last = n - 1; last >= 1; last--)
+    {
+        // At this point, theArray[last+1..n-1] is sorted, and its
+        // entries are greater than those in theArray[0..last].
+        // Select the largest entry in theArray[0..last]
+        int largest = findIndexOfLargest(theArray, last+1);
+
+        // Swap the largest entry, theArray[largest], with
+        // theArray[last]
+        std::swap(theArray[largest], theArray[last]);
+    }
+} // end selection sort
 
