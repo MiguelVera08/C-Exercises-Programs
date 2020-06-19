2ac91d5f467cee43807734d2ef49a2406ed7d196
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-19 09:28:00 -0600

    Added parameter breakdown and example of a call

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -6,4 +6,16 @@
 template<class ItemType>
 void insertionSort(ItemType theArray[], int n);
 
-
+/** Sorts the items in an array into ascending order.
+ @pre  None.
+ @post  theArray is sorted into ascending order; n is unchanged.
+ @param theArray  The given array.
+ @param n  The size of theArray. */
+//int main()
+//{
+//std::string a[6] = {"Z", "X", "R", "K", "F", "B"};
+//insertionSort(a, 6);
+//for (int i = 0; i < 6; i++)
+//std::cout << a[i] << " ";
+//std::cout << std::endl;
+//}  // end main
