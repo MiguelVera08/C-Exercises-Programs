8d8dd22b97740f5c65ba3bce8bd7327e285ee217
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-19 09:48:00 -0600

    Added expected ouput example the call to nextItem

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -20,6 +20,8 @@ void insertionSort(ItemType theArray[], int n);
 //std::cout << std::endl;
 //}  // end main
 
+// Expected outcome B F K R X Z
+
 template<class ItemType>
 void insertionSort(ItemType theArray[], int n){
 
@@ -32,5 +34,20 @@ void insertionSort(ItemType theArray[], int n){
     // unsorted region theArray[unsorted..n-1]
     for (int unsorted = 1; unsorted < n; unsorted++){
 
+        // At this point, theArray[0..unsorted-1] is sorted.
+        // Find the right position (loc) in theArray[0..unsorted]
+        // for theArray[unsorted], which is the first entry in the
+        // unsorted region; shift, if necessary, to make room
+        ItemType nextItem = theArray[unsorted];
+        int loc = unsorted;
+        while ((loc > 0) && (theArray[loc - 1] > nextItem))
+        {
+            // Shift theArray[loc - 1] to the right
+            theArray[loc] = theArray[loc - 1];
+            loc--;
+        }  // end while
+
+        // At this point, theArray[loc] is where nextItem belongs
+        theArray[loc] = nextItem; // Insert nextItem into sorted region
     }
 }
