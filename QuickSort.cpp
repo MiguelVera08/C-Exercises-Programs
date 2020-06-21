98eaf319608fb324db41d093b92d3d085864f5ba
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-21 10:24:00 -0600

    Added parameter breakdown and global variable for minimum size array quicksort sorts

---
+++ QuickSort.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+#include <string>
+
+static const int MIN_SIZE  = 10; // Smallest size of an array that quicksort will sort
+
+/** Sorts the items in an array into ascending order.
+ @pre  None.
+ @post  theArray is sorted into ascending order; n is unchanged.
+ @param theArray  The given array.
+ @param n  The size of theArray. */
