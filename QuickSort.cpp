df5293f2c6025318a73efa8a86b3681f329f994c
Miguel Vera <miguel.vera7908@gmail.com>
2020-06-21 10:47:00 -0600

    Order ShellSort will go in for loop

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -10,3 +10,19 @@ static const int MIN_SIZE  = 10; // Smallest size of an array that quicksort wil
  @post  theArray is sorted into ascending order; n is unchanged.
  @param theArray  The given array.
  @param n  The size of theArray. */
+
+template<class ItemType>
+void insertionSort(ItemType theArray[], int first, int last)
+{
+    // unsorted = first index of the unsorted region,
+    // loc = index of insertion in the sorted region,
+    // nextItem = next item in the unsorted region.
+    // Initially, sorted region is theArray[0],
+    // unsorted region is theArray[1..n-1].
+    // In general, sorted region is theArray[0..unsorted-1],
+    // unsorted region theArray[unsorted..n-1]
+    for (int unsorted = first + 1; unsorted <= last; unsorted++)
+    {
+
+    }
+}
