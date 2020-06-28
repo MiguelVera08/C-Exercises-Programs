385d29dcbf12211af682dde14a1b43962a10c3e3
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-06-27 23:51:55 -0500

    Practice with sum of numbers

--- Practice.cpp
+++ Practice.cpp
@@ -3,9 +3,13 @@ using namespace std;
  
 int main()
 {
-    cout << "\n\n Practice printing a message :\n";
-	  cout << "----------------------------------------------\n";
-    cout << " Welcome to Codeup\n" ;
-    cout << " Europa "<<endl ;
+    	cout << "\n\n Practice printing a message :\n";
+    	cout << "----------------------------------------------\n";
+    	cout << " Welcome to Codeup\n" ;
+    	cout << " Europa "<<endl ;
     
+	//Sum two numbers
+	cout << "\n\n Print the sum of two numbers :\n";
+	cout << "-----------------------------------\n";
+    	cout << " The sum of 29 and 30 is : "<< 29+30 <<"\n\n" ;
 }
