7c30192bdfb3cf12828a391e7aa34e3a438204dc
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-06-27 23:49:25 -0500

    Practice file to practice simple C++ functions

---
+++ Practice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "\n\n Practice printing a message :\n";
+	  cout << "----------------------------------------------\n";
+    cout << " Welcome to Codeup\n" ;
+    cout << " Europa "<<endl ;
+    
+}
