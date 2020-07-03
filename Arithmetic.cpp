2a61a02589140f689970f25ebafae4edbc4c2afc
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-02 21:27:35 -0500

    Update Arithmetic.cpp

--- Arithmetic.cpp
+++ Arithmetic.cpp
@@ -18,5 +18,12 @@
 #include <iomanip>   // formatting floating-point numbers with 1 decimal place
 using namespace std;
 
-
+int main() 
+{
+   int m1 = 5, m2 = 7;
+   double d1 = 3.7, d2 = 8.0;
+   
+    cout << "\n\n Display arithmetic operations with mixed data type :\n";
+	cout << "---------------------------------------------------------\n";
+   cout << fixed << setprecision(1);
 
