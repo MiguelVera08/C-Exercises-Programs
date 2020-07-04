3866afbfe4f454282e5f170f3d42f3dbfacd1fbc
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-03 22:25:11 -0500

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -2,3 +2,9 @@
 
 #include <iostream>  
 using namespace std;
+
+int main() {  
+  int n1=0,n2=1,n3,i,number;    
+  cout<<"Enter the number of elements: ";    
+  cin>>number;    
+  cout<<n1<<" "<<n2<<" "; //printing 0 and 1
