01a4296cb7ab3d893f67ba8711c6efdf8224d22f
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-06 19:41:50 -0500

    Created Armstrong program summary explanation

---
+++ Armstrong.cpp
@@ -0,0 +1,9 @@
+//Armstrong Number program to check if number input is Armstrong or not. An Armstrong number is a number equal to the sum ob its digits.
+//For example 0,1,153,370,371,407
+//
+//371 = (3*3*3)+(7*7*7)+(1*1*1)    
+//where:    
+//(3*3*3)=27    
+//(7*7*7)=343    
+//(1*1*1)=1    
+//So: 27+343+1=371 
