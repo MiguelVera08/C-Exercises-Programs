e082be449cf3bcb5091fbe3d8ffb15ae1e3633a5
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-06 19:46:19 -0500

    Create variables

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -7,3 +7,24 @@
 //(7*7*7)=343    
 //(1*1*1)=1    
 //So: 27+343+1=371 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int x,y,sum = 0, temp;
+  
+  //Get input from user
+  cout<<"Enter a Number: ";
+  
+  cin>>x;
+  temp = x;
+  
+  while(x > 0){
+    
+    y = x % 10;
+    
+    sum = sum + (y*y*y);
+    
+  }
+}
