4330cee45bece36463e18ccfd1a11c49b5909fb6
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-07 22:05:17 -0500

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,4 +3,11 @@
 
 #include <iostream>  
 using namespace std;  
-int main()
+int main(){
+  int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;    
+  cout<<"enter the number of row=";    
+  cin>>r;    
+  cout<<"enter the number of column=";    
+  cin>>c;    
+  cout<<"enter the first matrix element=\n";    
+ 
