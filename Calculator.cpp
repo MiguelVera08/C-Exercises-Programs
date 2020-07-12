38b3fd2eede19b3b5338d25546846da373318004
MiguelVera08 <57374158+MiguelVera08@users.noreply.github.com>
2020-07-11 21:54:07 -0500

    Description for Calculator exercise

---
+++ Calculator.cpp
@@ -0,0 +1,7 @@
+//Create a calculator that takes a number, math operater ex... +,-,*,/,^ and a second number from user then print
+//the result.
+//Example:
+//   5 + 3 = 8
+
+#include<iostream>
+using namespace std;
